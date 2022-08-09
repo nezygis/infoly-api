f2c2a5027796f2c48a255cf02f1ac8242989dfbf
Zygimantas Skuolis <zygimantas.skuolis@gmail.com>
2022-08-09 21:05:05 +0300

    Init project from spring initializr

---
+++ src/main/java/com/silouks/infoly/InfolyApplication.java
@@ -0,0 +1,13 @@
+package com.silouks.infoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfolyApplication.class, args);
+	}
+
+}
